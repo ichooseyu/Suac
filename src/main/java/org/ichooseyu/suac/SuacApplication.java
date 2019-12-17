54a655daa285188cae97baf018e67649acb3ae5f
ichoooseyu <ichooseyu@outlook.com>
2019-12-17 22:20:09 +0800

    新建项目

---
+++ src/main/java/org/ichooseyu/suac/SuacApplication.java
@@ -0,0 +1,13 @@
+package org.ichooseyu.suac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ichooseyu/suac/SuacApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ichooseyu.suac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
