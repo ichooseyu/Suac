4512994dc06bc77f070c9ce170b6b6c042a3f1af
chenhang <ichooseyu@outlook.com>
2020-03-23 10:23:51 +0800

    test master

--- src/main/java/org/ichooseyu/suac/core/service/UserDao.java
+++ src/main/java/org/ichooseyu/suac/core/service/UserDao.java
@@ -5,6 +5,6 @@ package org.ichooseyu.suac.core.service;
  */
 public class UserDao {
   public static void main(String[] args) {
-    System.out.println("test1");
+    System.out.println("test2");
   }
 }
